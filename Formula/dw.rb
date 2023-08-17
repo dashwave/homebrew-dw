1897d09d12f5a834251df4fc13322ce4a9c7b191
Supratik Das <rick.das08@gmail.com>
2023-08-17 16:02:10 +0530

    Fix class based definitions

--- Formula/dw.rb
+++ Formula/dw.rb
@@ -7,61 +7,55 @@ class Dw < Formula
     require "open-uri"
     require "digest/sha2"
     
-    class << self
-      def latest_release_info
-        uri = URI.parse("https://api.github.com/repos/dashwave/toolkits/releases/latest")
-        response = Net::HTTP.get_response(uri)
-        
-        unless response.is_a?(Net::HTTPSuccess)
-          raise "Could not fetch latest release info"
-        end
-        
-        JSON.parse(response.body)
+    def self.latest_release_info
+      uri = URI.parse("https://api.github.com/repos/dashwave/toolkits/releases/latest")
+      response = Net::HTTP.get_response(uri)
+      
+      unless response.is_a?(Net::HTTPSuccess)
+        raise "Could not fetch latest release info"
       end
       
-      def latest_release_url
-        release_info = latest_release_info()
-        assets = release_info['assets']
-        
-        os, arch = if OS.mac?
-                     if Hardware::CPU.arm?
-                       ['darwin', 'arm64']
-                     else
-                       ['darwin', 'amd64']
-                     end
-                   elsif OS.linux?
-                     if Hardware::CPU.arm?
-                       ['linux', 'arm64']
-                     else
-                       ['linux', 'amd64']
-                     end
-                   else
-                     raise "Unsupported OS"
-                   end
-    
-        asset = assets.find do |a|
-          a['browser_download_url'].include?("#{os}_#{arch}")
-        end
-        
-        raise "Could not find appropriate asset for release" if asset.nil?
-        
-        asset['browser_download_url']
-      end
-      
-      def latest_release_sha
-        url = latest_release_url()
-        sha256 = Digest::SHA256.file(URI.parse(url).open).hexdigest
-        sha256
-      end
-      
-      def version
-        latest_release_info['tag_name']
-      end
+      JSON.parse(response.body)
     end
     
-    url Dw.latest_release_url
-    sha256 Dw.latest_release_sha
-    version Dw.version
+    def self.latest_release_url
+      release_info = self.latest_release_info()
+      assets = release_info['assets']
+      
+      os, arch = if OS.mac?
+                   if Hardware::CPU.arm?
+                     ['darwin', 'arm64']
+                   else
+                     ['darwin', 'amd64']
+                   end
+                 elsif OS.linux?
+                   if Hardware::CPU.arm?
+                     ['linux', 'arm64']
+                   else
+                     ['linux', 'amd64']
+                   end
+                 else
+                   raise "Unsupported OS"
+                 end
+  
+      asset = assets.find do |a|
+        a['browser_download_url'].include?("#{os}_#{arch}")
+      end
+      
+      raise "Could not find appropriate asset for release" if asset.nil?
+      
+      asset['browser_download_url']
+    end
+    
+    def self.latest_release_sha
+      url = self.latest_release_url()
+      sha256 = Digest::SHA256.file(URI.parse(url).open).hexdigest
+      sha256
+    end
+    
+    url self.latest_release_url
+    sha256 self.latest_release_sha
+    version self.latest_release_info['tag_name']
     
     depends_on "rsync"
     depends_on "esolitos/ipa/sshpass"
@@ -70,5 +64,5 @@ class Dw < Formula
     def install
       bin.install "dw"
     end
-  end
+end
   
